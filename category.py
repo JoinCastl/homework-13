38350a852b7253760382ee2331a30268fffaf425
JoinCastl <danilsevcenko759@gmail.com>
2024-03-19 19:34:45 +0300

    glhf

--- category.py
+++ category.py
@@ -1,19 +1,20 @@
+from typing import List
 class Category:
     name: str
     description: str
-    products: str
-    total_categories = 0
-    total_unique_products = 0
+    products: list
+    total_categories: int = 0
+    total_unique_products: int = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.products = products
         Category.total_categories += 1
+        Category.total_unique_products += len(products)
+
 
     def add_product(self, product):
         self.products.append(product)
-        Category.total_unique_products.add(product.name)
-    def __str__(self):
-        products_str = "\n".join([str(product) for product in self.products])
-        return f"Category: {self.name}\nDescription: {self.description}\nProducts:\n{products_str}"
+        Category.total_unique_products += 1
+
--- product.py
+++ product.py
@@ -4,11 +4,9 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.price = float(price)
         self.quantity = quantity
 
-    def __str__(self):
-        return f"Product: {self.name} - {self.description}, Price: {self.price}, Quantity: {self.quantity}"
